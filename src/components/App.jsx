7e3e06dff0724a529c11aaf4f929c4dd4f3048f6
Hrustch <nik.stalker47@gmail.com>
2023-05-31 20:23:25 +0300

    formating code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,53 @@
-import { Component } from 'react'
+import { Component } from 'react';
 import { FeedbackOptions } from './Feedback/FeedbackOptions';
 import { FeedbackStatistic } from './Feedback/FeedbackStatistic';
 import { Section } from './Feedback/Section';
 import { Notification } from './Feedback/Notification';
 
 export default class App extends Component {
-  state = {good: 0, neutral: 0, bad: 0}
+  state = { good: 0, neutral: 0, bad: 0 };
 
-  countTotalFeedback = ()=>{
-    return Object.values.reduce((acc, count)=>(acc + count), 0)   
-  }
-  countPositiveFeedbackPercentage = ()=>{
-    if (this.countTotalFeedback() > 0){
-      return Math.round(this.state.good/this.countTotalFeedback()*100)
+  countTotalFeedback = () => {
+    return Object.values.reduce((acc, count) => acc + count, 0);
+  };
+  countPositiveFeedbackPercentage = () => {
+    if (this.countTotalFeedback() > 0) {
+      return Math.round((this.state.good / this.countTotalFeedback()) * 100);
     }
     return 0;
-  }
-  onLeaveFeedback = (event)=>{
-    const {name} = event.target
+  };
+  onLeaveFeedback = event => {
+    const { name } = event.target;
     this.setState({
-      [name]: this.state[name]+1
-    })
-  }
+      [name]: this.state[name] + 1,
+    });
+  };
 
   render() {
-    const options = Object.keys(this.state)
-    const total = this.countTotalFeedback()
-    const posFeedback = this.countPositiveFeedbackPercentage()
+    const options = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    const posFeedback = this.countPositiveFeedbackPercentage();
     return (
       <div>
-        <Section title = "Please leave feedback">
+        <Section title="Please leave feedback">
           <FeedbackOptions
-            options = {options}
-            onLeaveFeedback = {this.onLeaveFeedback}
-          />          
-        </Section>
-        {total > 0 ? <Section title = "Statistic"><FeedbackStatistic
-            options = {options}
-            state = {this.state}
-            total = {total}
-            posFeedback = {posFeedback}
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
           />
-        </Section> : <Notification massage="Please, leave your masage!"/>}    
-      </div>      
-    )
+        </Section>
+        {total > 0 ? (
+          <Section title="Statistic">
+            <FeedbackStatistic
+              options={options}
+              state={this.state}
+              total={total}
+              posFeedback={posFeedback}
+            />
+          </Section>
+        ) : (
+          <Notification massage="Please, leave your masage!" />
+        )}
+      </div>
+    );
   }
 }
